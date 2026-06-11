f02d219ea05199882b21d68c87ae47f9f46140fb
NurainSyazwani <wanieyhoney1403@gmail.com>
2026-06-12 01:27:24 +0800

    Fix Render port binding

--- app.py
+++ app.py
@@ -669,4 +669,5 @@ def encyclopedia():
 # RUN APP
 # =========================
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
